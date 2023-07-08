793d117437cc59da2a054f5684e4146a3c0d5aed
Adrian Carpio Moran <adriancarpiomoran@gmail.com>
2023-07-08 13:12:44 -0500

    Proyecto base asistente virtual app

---
+++ src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,90 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' }
+  },
+  {
+    name: 'Buttons',
+    url: '/buttons',
+    iconComponent: { name: 'cil-cursor' },
+    children: [
+      {
+        name: 'Buttons',
+        url: '/buttons/buttons'
+      },
+      {
+        name: 'Button groups',
+        url: '/buttons/button-groups'
+      },
+      {
+        name: 'Dropdowns',
+        url: '/buttons/dropdowns'
+      },
+    ]
+  },
+  {
+    name: 'Forms',
+    url: '/forms',
+    iconComponent: { name: 'cil-notes' },
+    children: [
+      {
+        name: 'Form Control',
+        url: '/forms/form-control'
+      },
+      {
+        name: 'Select',
+        url: '/forms/select'
+      },
+      {
+        name: 'Checks & Radios',
+        url: '/forms/checks-radios'
+      },
+      {
+        name: 'Range',
+        url: '/forms/range'
+      },
+      {
+        name: 'Input Group',
+        url: '/forms/input-group'
+      },
+      {
+        name: 'Floating Labels',
+        url: '/forms/floating-labels'
+      },
+      {
+        name: 'Layout',
+        url: '/forms/layout'
+      },
+      {
+        name: 'Validation',
+        url: '/forms/validation'
+      }
+    ]
+  },
+  {
+    name: 'Pages',
+    url: '/login',
+    iconComponent: { name: 'cil-star' },
+    children: [
+      {
+        name: 'Login',
+        url: '/login'
+      },
+      {
+        name: 'Register',
+        url: '/register'
+      },
+      {
+        name: 'Error 404',
+        url: '/404'
+      },
+      {
+        name: 'Error 500',
+        url: '/500'
+      }
+    ]
+  },
+];
